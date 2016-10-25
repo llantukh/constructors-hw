eae5dba48923f992849cb344c700c48079ab5eb8
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-25 14:14:18 -0400

    fixed all tests

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,14 @@
-function Dog () {
+function Dog (options) {
+  var options = options || {};
+  this.status = "normal" || options.status;
+  this.color = options.color;
+  // moonshine doens't have a hungry option so we need it to go to the default
+  if (options.hungry === undefined) {
+    this.hungry = true;
+  } else {
+    this.hungry = options.hungry;
+  }
+
 }
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,20 @@
-function Person () {
+function Person (options) {
+  options = options || {};
+  if (options.cool === undefined) {
+    this.cool = false;
+  } else {
+    this.cool = options.cool;
+  }
 };
+
+Person.prototype.pet = function (dog) {
+  dog.status = "happy";
+};
+
+Person.prototype.feed = function (dog) {
+  dog.hungry = false;
+};
+
+
 
 export { Person };
